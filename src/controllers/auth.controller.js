91bf6ac426fc1dfcf94777f1a17550f9d2b5a570
AndresManjarres <89228795+AndresManjarres@users.noreply.github.com>
2023-12-05 00:01:26 -0500

    Implemetacion bcript(Contraseña) y token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,35 @@
 import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+import { crearToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+    //Uso de la libreria bcryptjs para encriptar la contraseña
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    const token = await crearToken({id: userSaved._id});
+
+    //Generar token
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
