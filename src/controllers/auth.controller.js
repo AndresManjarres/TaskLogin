7d8ffbde2d8407468da6b7e867f4d84570d9f446
AndresManjarres <89228795+AndresManjarres@users.noreply.github.com>
2023-12-05 12:27:58 -0500

    Regitro, Login y Logout finished

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,6 +33,39 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+
+    const userFound = await User.findOne({email});
+
+    if(!userFound) return res.status(400).json({message: "Usuario no encontrado"});
+
+    //Comparar contraseñas
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if(!isMatch) return res.status(400).json({message: "Contraseña incorrecta"});
+
+    const token = await crearToken({id: userFound._id});
+
+    //Generar token
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {expires: new Date(0)});
+
+  return res.sendStatus(200);
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 
 //Permite crear peticiones tipo GET, POST, PUT, DELETE.
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
