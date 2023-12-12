b3e7ce4b62e61fd21840f8998e29b7700f95ce67
AndresManjarres <89228795+AndresManjarres@users.noreply.github.com>
2023-12-11 19:37:10 -0500

    Carpeta client para el FrontEnd

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Home Page</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/register' element={<h1>Registro</h1>} />
+          <Route path='/tasks' element={<h1>Task Page</h1>} />
+          <Route path='/add-task' element={<h1>NewTask</h1>} />
+          <Route path='/tasks/:id' element={<h1>UpdateTask</h1>} />
+          <Route path='/profile' element={<h1>Perfil</h1>} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
